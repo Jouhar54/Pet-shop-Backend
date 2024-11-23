d54d153d9a373cd2fbe389ab74f563821ebe4395
Jouhar <jouharvaramangalath786@gmail.com>
2024-11-23 15:49:50 +0530

    feat: rate limiter implimented

--- server.js
+++ server.js
@@ -4,18 +4,26 @@ const express = require('express');
 const connectDB = require('./config/db');
 const userRouter = require('./routes/userRouter');
 const router = require('./routes/adminRouter');
+const rateLimit = require('express-rate-limit');
 
 const app = express();
 
+const apiLimiter = rateLimit({
+    windowMs: 5 * 60 * 1000, 
+    max: 50,
+    message: 'Too many requests from this IP, please try again later.',
+    standardHeaders: true,
+    legacyHeaders: false, 
+  });
+  
 config();
 connectDB();
 
+app.use(apiLimiter);
 app.use(cors());
-
 app.use(express.json())
 
 app.use('/api/users', userRouter);
-
 app.use('/api/admin', router)
 
 const PORT = process.env.PORT || 3000 ;
